67115629032f4752915e78b72c90899914f2a82d
Vijay Khemlani <vkhemlan@gmail.com>
2012-01-27 15:26:04 -0300

    Added EMAIL and MEDIA settings to defaults

--- settings/base.py
+++ settings/base.py
@@ -1,4 +1,10 @@
 from settings.defaults import *
 
 SECRET_KEY = '1i(zariw^c&gd3j+cfvdwkz9weqwamilkf)kxag@9wpk_$+bzm'
-MEDIA_URL = 'http://localhost:8000/media/'
+MEDIA_URL = 'http://materialmayor.bomberos.cl/media/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'something@gmail.com'
+EMAIL_HOST_PASSWORD = 'something'
+EMAIL_USE_TLS = True
