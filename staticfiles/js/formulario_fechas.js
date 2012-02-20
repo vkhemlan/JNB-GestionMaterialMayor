79b130f0c37783a94287d4f2a31fb31eb76f0f0f
Vijay Khemlani <vkhemlan@gmail.com>
2012-02-20 10:59:19 -0300

    Removed static files generated by collectstatic

--- staticfiles/js/formulario_fechas.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $.datepicker.setDefaults($.datepicker.regional['es']);
-    $('.datepicker').datepicker({
-        changeMonth: true,
-        changeYear: true,
-        yearRange: 'c-20:c'
-    })
-})
--- staticfiles/js/notifications.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	$('.success').slideDown().delay(3000).slideUp();
-	$('.error').slideDown().delay(3000).slideUp();
-	$('.notice').slideDown().delay(3000).slideUp();
-
-    $('.tablesorter').tablesorter().tablesorterPager({container: $("#pager")});
-});
