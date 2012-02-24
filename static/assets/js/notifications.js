37fab027b9e3fd61b97c176ea04e34a8c1132b7b
Vijay Khemlani <vkhemlan@gmail.com>
2012-02-24 09:46:14 -0300

    Agregada busqueda rapida de material mayor y paginacion simplificada

--- static/assets/js/notifications.js
+++ static/assets/js/notifications.js
@@ -3,5 +3,9 @@ $(function() {
 	$('.error').slideDown().delay(3000).slideUp();
 	$('.notice').slideDown().delay(3000).slideUp();
 
-    $('.tablesorter').tablesorter().tablesorterPager({container: $("#pager")});
+    $('.tablesorter').tablesorter()
+    .tablesorterPager({container: $("#pager")})
+    .tablesorterFilter({filterContainer: "#filter-box",
+                            filterClearContainer: "#filter-clear-button",
+                            filterColumns: [0,1,2,3,4,5,6]});
 });
